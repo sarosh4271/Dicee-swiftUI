482ae512ae6a20a57cf074be409dd477cf96c0a5
sarosh4271 <74011881+sarosh4271@users.noreply.github.com>
2022-01-03 22:18:19 +0500

    Initial Commit

---
+++ dicee-swiftUI/dicee_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_swiftUIApp.swift
+//  dicee-swiftUI
+//
+//  Created by Sarosh Tahir on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct dicee_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
